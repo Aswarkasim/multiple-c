dbecc8d90533984429d748d95e031abdcf654f88
aswarkasim <aswarkasim@gmail.com>
2022-05-19 13:18:53 +0700

    test

--- app/Http/Controllers/AdminQuestionController.php
+++ app/Http/Controllers/AdminQuestionController.php
@@ -11,7 +11,7 @@ class AdminQuestionController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     *adkaah
      * @return \Illuminate\Http\Response
      */
     public function index()
